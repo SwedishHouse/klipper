0b19cee38d63a98377445318f42e58ae413d0126
SwedishHouse <swedish2015@yandex.ru>
2024-10-23 23:50:04 +0800

    pins created but stepper don't work

--- klippy/extras/g_nome_thc.py
+++ klippy/extras/g_nome_thc.py
@@ -48,13 +48,39 @@ class THC:
 
     def __init__(self, config) -> None:
         self.printer = config.get_printer()
-        self.manual_stepper = None
+        # self.manual_stepper = None
         self.__arc_pin = None
-        self.toolhead = None
+        # self.toolhead = None
+        dir_pin, enable_pin = config.get('dir_pin'), config.get('enable_pin')
+        pins = {'dir_pin':dir_pin, 'enable_pin': enable_pin}
+        ppins = self.printer.lookup_object('pins')
+        for key, val in pins.items():
+            ppins.allow_multi_use_pin(val)
+            setattr(self, key, ppins.setup_pin('digital_out', val))
         self.buttons = self.printer.load_object(config, "buttons")
+        # Stepper pins
+        # Step 
+        self.pwn_pin = config.get('pin_pwm')  # Like step
+        self.cycle_time = config.getfloat('cycle_time', 0.01)
+        self.max_power = config.getfloat('max_power', 1.0)
+        self.pwm_pin = ppins.setup_pin('pwm', self.pwn_pin)
+        self.pwm_pin.setup_cycle_time(self.cycle_time)
+        self.pwm_pin.setup_max_duration(2)
+        # self.pwm_pin.setup_pwm(self.cycle_time, self.max_power)
+
+        # Direction
+        # self.dir_pin = config.get('dir_pin') 
+        # self.dir_pin = self.printer.lookup_pin(self.dir_pin)
+        # # self.dir_pin.setup_pin('digital_out')
+        # # Enable pin
+        # self.enable_pin = config.get('enable_pin') 
+        # self.enable_pin = self.printer.lookup_pin(self.enable_pin)
+
+        # THC pins
         self.pin_up = config.get('pin_up')
         self.pin_down = config.get('pin_down')
-        # self.buttons.register_buttons([self.pin_up], self.button_up_callback)
+
+        self.buttons.register_buttons([self.pin_up], self.button_up_callback)
         self.buttons.register_buttons([self.pin_down], self.button_down_callback)
         # self.stepper_thc = Stepper_THC(config)
         self.name = "THC"
@@ -85,6 +111,15 @@ class THC:
         self._is_ready = True
         pass 
 
+    def set_pwm(self, value):
+        self.pwm_pin.set_pwm(100, value)
+    
+    def set_dir_pin(self, value):
+        self.dir_pin.set_digital(value)
+
+    def set_enable_pin(self, value):
+        self.enable_pin.set_digital(value)
+    
     def button_up_callback(self, eventtime, state):
         # try:
         #     if self.toolhead is not None and not state:
@@ -94,19 +129,21 @@ class THC:
         # except Exception as e:
         #     print(str(e))
         if state and self._is_ready:
-            try:   
-
-                position = self.toolhead.get_position()[0]
-                print(position)
-                print(self.manual_stepper.get_position())
-                self.manual_stepper.do_set_position(0)
+            try:
+                # self.set_enable_pin(1)
+                # self.set_dir_pin(1)
+                self.set_pwm(0.5)
+                # position = self.toolhead.get_position()[0]
+                # print(position)
+                # print(self.manual_stepper.get_position())
+                # self.manual_stepper.do_set_position(0)
                 # self.manual_stepper.do_enable(0)
                 # print(f'Pressed UP at {position}')
                 # self.stepper_thc.running = True
                 # self.gcode.run_script_from_command('M400')
                 # self.gcode.run_script_from_command(f"MANUAL_STEPPER STEPPER=thc_stepper ENABLE=0 SET_POSITION=0 MOVE={-10} SYNC=0")
-                self.manual_stepper.flush_step_generation()
-                self.manual_stepper.do_move(position - 10, 50, 200, False)
+                # self.manual_stepper.flush_step_generation()
+                # self.manual_stepper.do_move(position - 10, 50, 200, False)
                 
                 pass
             except Exception as e:
@@ -127,17 +164,20 @@ class THC:
         # if state and self._is_ready and self.__arc_pin.get_status(self.reactor.NOW):
         if state and self._is_ready:
             try:
-                position = self.toolhead.get_position()[0]
-                print(position)
-                print(self.manual_stepper.get_position())
-                self.manual_stepper.do_set_position(0)
+                # self.set_enable_pin(0)
+                # self.set_dir_pin(0)
+                self.set_pwm(0.5)
+                # position = self.toolhead.get_position()[0]
+                # print(position)
+                # print(self.manual_stepper.get_position())
+                # self.manual_stepper.do_set_position(0)
                 # self.manual_stepper.do_enable(0)
                 # print(f'Pressed Down at {position}')
                 # self.stepper_thc.running = True
                 # self.gcode.run_script_from_command('M400')
                 # self.gcode.run_script_from_command(f"MANUAL_STEPPER STEPPER=thc_stepper MOVE=10 SET_POSITION=0 SPEED=50 SYNC=0")
-                self.manual_stepper.flush_step_generation()
-                self.manual_stepper.do_move(position + 10, 50, 200, False)
+                # self.manual_stepper.flush_step_generation()
+                # self.manual_stepper.do_move(position + 10, 50, 200, False)
                 
                 pass
             except Exception as e:
@@ -149,10 +189,10 @@ class THC:
 
     def _connect_event(self):
         try:
-            self.toolhead = self.printer.lookup_object('toolhead')
+            # self.toolhead = self.printer.lookup_object('toolhead')
             # self.toolhead = self.printer.lookup_object('stepper_z')
             # self.toolhead.manual_move([None, None, 10], self.toolhead.get_max_velocity())
-            self.manual_stepper = self.printer.lookup_object('manual_stepper thc_stepper')
+            # self.manual_stepper = self.printer.lookup_object('manual_stepper thc_stepper')
             self.__arc_pin = self.printer.lookup_object('output_pin arc_on')
             
             print('Yes')
